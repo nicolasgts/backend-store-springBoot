b6ef4c7816cb3429fcced43d6e79533953bce007
Nicolas Gabriel <nicolasgabrielt@gmail.com>
2018-04-21 01:46:41 -0300

    Testing the first domain's class

--- src/main/java/com/nicolas/cursomc/resourses/CategoriaResource.java
+++ src/main/java/com/nicolas/cursomc/resourses/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.nicolas.cursomc.resourses;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nicolas.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST is working";
+	public List<Categoria> listar() {
+		
+		Categoria categoria1 = new Categoria(1,"Informática");
+		Categoria categoria2 = new Categoria(2,"Escritório");
+		
+		 List<Categoria> lista = new ArrayList<>();
+		 lista.add(categoria1);
+		 lista.add(categoria2);
+		 
+		return lista;
 	}
 }
