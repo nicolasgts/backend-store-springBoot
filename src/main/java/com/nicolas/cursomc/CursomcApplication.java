6b1e827eb1073b9de5c546e8d4d8340a2a510110
Nicolas Gabriel <nicolasgabrielt@gmail.com>
2018-04-23 23:55:55 -0300

    Product created and association in BD many to many

--- src/main/java/com/nicolas/cursomc/CursomcApplication.java
+++ src/main/java/com/nicolas/cursomc/CursomcApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.nicolas.cursomc.domain.Categoria;
+import com.nicolas.cursomc.domain.Produto;
 import com.nicolas.cursomc.repositories.CategoriaRepository;
+import com.nicolas.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
@@ -17,6 +19,8 @@ public class CursomcApplication implements CommandLineRunner {
 	 the BD when the application get started */
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -31,9 +35,24 @@ public class CursomcApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador" , 2000.00);
+		Produto p2 = new Produto(null, "Impressora" , 800.00);
+		Produto p3 = new Produto(null, "Mouse" , 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat1.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().add(cat1);
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().add(cat1);
+		
+		
+		
 		// this line is different of the implementation in Spring Boot 1.5
 		// there would be categoriaRepository.save(Arrays.asList(cat1,cat2));
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
 	}
 }
